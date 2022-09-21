0d92b2e8cdabae2d52a7b76ff2cbb956de173b89
Siddharth Godhania <siddharth.godhania@capgemini.com>
2022-09-21 11:36:00 +0100

    MPDP-119 Fixing tests and added precommit hook

---
+++ test/utils/footer-expects.js
@@ -0,0 +1,15 @@
+const expectedList = ['Accessibility', 'Cookies', 'Privacy']
+const toBePresent = ($) => {
+  const footerList = []
+  $('.govuk-footer__link').each((_index, value) => {
+    footerList.push($(value).attr('href'))
+  })
+  expect(
+    expectedList.length === footerList.length &&
+    expectedList.every(x => footerList.includes(x))
+  ).toEqual(true)
+}
+
+module.exports = {
+  toBePresent
+}
