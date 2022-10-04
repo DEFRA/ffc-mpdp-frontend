61fe15e57feb63acc1b439bb837b97a18e9d755b
Siddharth Godhania <siddharth.godhania@capgemini.com>
2022-10-04 15:36:17 +0100

    MPDP-157 Readding tests after typescript setup

---
+++ test/integration/narrow/routes/layout/index.test.ts
@@ -0,0 +1,27 @@
+import config from '../../../../../app/config'
+import * as cheerio from 'cheerio'
+import { expectPhaseBanner } from '../../../../utils/phase-banner-expect'
+import { expectFooter } from '../../../../utils/footer-expects'
+
+describe('MPDP layout test', () => {
+  test('Primary url route returns 200', async () => {
+    const options = {
+      method: 'GET',
+      url: '/'
+    }
+
+    const res = await global.__SERVER__.inject(options)
+
+    expect(res.statusCode).toBe(200)
+    const $ = cheerio.load(res.payload)
+    expect($('.govuk-heading-l').text()).toEqual(
+      'Service Start Page'
+    )
+    const button = $('.govuk-main-wrapper .govuk-button')
+    expect(button.attr('href')).toMatch('/mpdp/search')
+    expect(button.text()).toMatch('Start now')
+    expect($('title').text()).toEqual(config.serviceName)
+    expectPhaseBanner($)
+    expectFooter($)
+  })
+})
