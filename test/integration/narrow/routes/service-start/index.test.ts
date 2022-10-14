924ae2d6763aed10f75ee8f9ee198446754dc12e
Siddharth Godhania <siddharth.godhania@capgemini.com>
2022-10-14 14:43:44 +0100

    MPDP-6 Adding service start page

--- test/integration/narrow/routes/service-start/index.test.ts
+++ test/integration/narrow/routes/service-start/index.test.ts
@@ -1,4 +1,6 @@
 import * as cheerio from 'cheerio'
+import { expectFooter } from '../../../../utils/footer-expects'
+import { expectHeader } from '../../../../utils/header-expects'
 import { expectPhaseBanner } from '../../../../utils/phase-banner-expect'
 
 describe('MPDP service start page test', () => {
@@ -9,15 +11,22 @@ describe('MPDP service start page test', () => {
     }
 
     const res = await global.__SERVER__.inject(options)
-
     expect(res.statusCode).toBe(200)
+
     const $ = cheerio.load(res.payload)
     expect($('.govuk-heading-l').text()).toEqual(
-      'Service Start Page'
+      'Find data on farm and land payments'
     )
+
     const button = $('.govuk-main-wrapper .govuk-button')
     expect(button.attr('href')).toMatch('/mpdp/search')
     expect(button.text()).toMatch('Start now')
+
+    expect($('#downloadAllLink').attr('href')).toEqual('/')
+    expect($('#capLink').attr('href')).toEqual('https://cap-payments.defra.gov.uk/Default.aspx')
+    
     expectPhaseBanner($)
+    expectFooter($)
+    expectHeader($)
   })
 })
