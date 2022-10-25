c6e136f908f0fb8ac975c954e6fd74dc1f1c37cb
Siddharth Godhania <siddharth.godhania@capgemini.com>
2022-10-25 11:01:50 +0100

    MPDP-108 Adding more refactoring changes

--- test/unit/backend/api.test.ts
+++ test/unit/backend/api.test.ts
@@ -1,11 +1,11 @@
-process.env.MPDP_BACKEND_ENDPOINT = 'http://__TEST_ENDPOINT__'
+const endpoint = 'http://__TEST_ENDPOINT__'
+process.env.MPDP_BACKEND_ENDPOINT = endpoint
+
 import wreck from '@hapi/wreck'
 import { get } from '../../../app/backend/api'
 
 describe('Backend API tests', () => {
-    const endpoint = 'http://__TEST_ENDPOINT__'
     const route = '/__TEST_ROUTE__'
-
     test('service uses the env variable to connect to the backend service', async () => {
         const mockGet = jest.fn()
         jest.spyOn(wreck, 'get').mockImplementation(mockGet)
