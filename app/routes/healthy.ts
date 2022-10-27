b4fcca9be59a925c79819cd6720acb3ac01fb445
Siddharth Godhania <siddharth.godhania@capgemini.com>
2022-10-27 14:04:40 +0100

    MPDP-108 Refactoring

--- app/routes/healthy.ts
+++ app/routes/healthy.ts
@@ -1,11 +1,7 @@
 import { Request, ResponseToolkit, ResponseObject } from "@hapi/hapi";
-import { get } from '../backend/api'
 
 module.exports = {
   method: 'GET',
   path: '/healthy',
-  handler: async (_request: Request, h: ResponseToolkit): Promise<ResponseObject> => {
-    const { res } = await get('/healthy')
-    return h.response(res.statusMessage).code(200)
-  }
+  handler: (_request: Request, h: ResponseToolkit): ResponseObject => h.response('ok').code(200)
 }
