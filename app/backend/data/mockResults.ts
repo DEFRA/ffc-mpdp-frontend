06bd7074eded4fb5f159ebefdac0f7d8de900d91
Siddharth Godhania <siddharth.godhania@capgemini.com>
2022-11-09 12:56:34 +0000

    MPDP-219 Adding some changes to dummy data

--- app/backend/data/mockResults.ts
+++ app/backend/data/mockResults.ts
@@ -1,157 +1,157 @@
 export default [
-    {
-      payee_name: 'T R Carter & Sons 1',
-      part_postcode: 'RG1',
-      town: 'Reading',
-      county_council: 'Berkshire',
-      amount: '£11,000000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 2',
-      part_postcode: 'RG1',
-      town: 'Reading',
-      county_council: 'Berkshire',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 3',
-      part_postcode: 'RG1',
-      town: 'Reading',
-      county_council: 'Berkshire',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 4',
-      part_postcode: 'RG1',
-      town: 'Reading',
-      county_council: 'Berkshire',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 5',
-      part_postcode: 'RG1',
-      town: 'Reading',
-      county_council: 'Berkshire',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 6',
-      part_postcode: 'SL4',
-      town: 'Winkfield',
-      county_council: 'Berkshire',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 7',
-      part_postcode: 'SL4',
-      town: 'Winkfield',
-      county_council: 'Berkshire',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 8',
-      part_postcode: 'SL4',
-      town: 'Winkfield',
-      county_council: 'Berkshire',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 9',
-      part_postcode: 'SL4',
-      town: 'Winkfield',
-      county_council: 'Berkshire',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 10',
-      part_postcode: 'SL4',
-      town: 'Winkfield',
-      county_council: 'Berkshire',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 11',
-      part_postcode: 'LS6',
-      town: 'Leeds',
-      county_council: 'Yorkshire and The Humber',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 12',
-      part_postcode: 'LS6',
-      town: 'Leeds',
-      county_council: 'Yorkshire and The Humber',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 13',
-      part_postcode: 'LS6',
-      town: 'Leeds',
-      county_council: 'Yorkshire and The Humber',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 14',
-      part_postcode: 'LS6',
-      town: 'Leeds',
-      county_council: 'Yorkshire and The Humber',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 15',
-      part_postcode: 'LS6',
-      town: 'Leeds',
-      county_council: 'Yorkshire and The Humber',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 16',
-      part_postcode: 'LS6',
-      town: 'Leeds',
-      county_council: 'Yorkshire and The Humber',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 17',
-      part_postcode: 'TS24',
-      town: 'Hartlepool',
-      county_council: 'Durham',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 18',
-      part_postcode: 'TS24',
-      town: 'Hartlepool',
-      county_council: 'Durham',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 19',
-      part_postcode: 'TS24',
-      town: 'Hartlepool',
-      county_council: 'Durham',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 20',
-      part_postcode: 'TS24',
-      town: 'Hartlepool',
-      county_council: 'Durham',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 21',
-      part_postcode: 'TS24',
-      town: 'Hartlepool',
-      county_council: 'Durham',
-      amount: '£10,000'
-    },
-    {
-      payee_name: 'T R Carter & Sons 22',
-      part_postcode: 'TS24',
-      town: 'Hartlepool',
-      county_council: 'Durham',
-      amount: '£10,000'
-    }
+  {
+    payee_name: 'T R Carter & Sons 1',
+    part_postcode: 'RG1',
+    town: 'Reading',
+    county_council: 'Berkshire',
+    amount: '£11,000000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 2',
+    part_postcode: 'RG1',
+    town: 'Reading',
+    county_council: 'Berkshire',
+    amount: '£10,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 3',
+    part_postcode: 'RG1',
+    town: 'Reading',
+    county_council: 'Berkshire',
+    amount: '£10,200'
+  },
+  {
+    payee_name: 'T R Carter & Sons 4',
+    part_postcode: 'RG1',
+    town: 'Reading',
+    county_council: 'Berkshire',
+    amount: '£10,743.55'
+  },
+  {
+    payee_name: 'T R Carter & Sons 5',
+    part_postcode: 'RG1',
+    town: 'Reading',
+    county_council: 'Berkshire',
+    amount: '£11,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 6',
+    part_postcode: 'SL4',
+    town: 'Winkfield',
+    county_council: 'Berkshire',
+    amount: '£50,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 7',
+    part_postcode: 'SL4',
+    town: 'Winkfield',
+    county_council: 'Berkshire',
+    amount: '£2,040'
+  },
+  {
+    payee_name: 'T R Carter & Sons 8',
+    part_postcode: 'SL4',
+    town: 'Winkfield',
+    county_council: 'Berkshire',
+    amount: '£10,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 9',
+    part_postcode: 'SL4',
+    town: 'Winkfield',
+    county_council: 'Berkshire',
+    amount: '£10,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 10',
+    part_postcode: 'SL4',
+    town: 'Winkfield',
+    county_council: 'Berkshire',
+    amount: '£10,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 11',
+    part_postcode: 'LS6',
+    town: 'Leeds',
+    county_council: 'Yorkshire and The Humber',
+    amount: '£20,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 12',
+    part_postcode: 'LS6',
+    town: 'Leeds',
+    county_council: 'Yorkshire and The Humber',
+    amount: '£30,500'
+  },
+  {
+    payee_name: 'T R Carter & Sons 13',
+    part_postcode: 'LS6',
+    town: 'Leeds',
+    county_council: 'Yorkshire and The Humber',
+    amount: '£12,330'
+  },
+  {
+    payee_name: 'T R Carter & Sons 14',
+    part_postcode: 'LS6',
+    town: 'Leeds',
+    county_council: 'Yorkshire and The Humber',
+    amount: '£10,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 15',
+    part_postcode: 'LS6',
+    town: 'Leeds',
+    county_council: 'Yorkshire and The Humber',
+    amount: '£10,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 16',
+    part_postcode: 'LS6',
+    town: 'Leeds',
+    county_council: 'Yorkshire and The Humber',
+    amount: '£10,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 17',
+    part_postcode: 'TS24',
+    town: 'Hartlepool',
+    county_council: 'Durham',
+    amount: '£10,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 18',
+    part_postcode: 'TS24',
+    town: 'Hartlepool',
+    county_council: 'Durham',
+    amount: '£10,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 19',
+    part_postcode: 'TS24',
+    town: 'Hartlepool',
+    county_council: 'Durham',
+    amount: '£10,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 20',
+    part_postcode: 'TS24',
+    town: 'Hartlepool',
+    county_council: 'Durham',
+    amount: '£10,000'
+  },
+  {
+    payee_name: 'T R Carter & Sons 21',
+    part_postcode: 'TS24',
+    town: 'Hartlepool',
+    county_council: 'Durham',
+    amount: '£11,200'
+  },
+  {
+    payee_name: 'T R Carter & Sons 22',
+    part_postcode: 'TS24',
+    town: 'Hartlepool',
+    county_council: 'Durham',
+    amount: '£122,300.25'
+  }
 
-  ]
+]
