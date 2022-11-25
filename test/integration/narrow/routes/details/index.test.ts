de2c3af99f90df9a621b11eedb0baab5baf3062a
Siddharth Godhania <110030290+SidG-CG@users.noreply.github.com>
2022-11-25 12:23:40 +0000

    MPDP-188 More Actions Section (#29)
    * MPDP-188 Adding more comments sesion
    
    * MPDP-188 Added more actions section
    
    * MPDP-188 Adding minor css changes
    
    * MPDP-19 Added feedback survery link
    
    * MPDP-19 Removed unused feedback route

--- test/integration/narrow/routes/details/index.test.ts
+++ test/integration/narrow/routes/details/index.test.ts
@@ -5,37 +5,56 @@ import { expectPhaseBanner } from '../../../../utils/phase-banner-expect'
 import { getOptions } from '../../../../utils/helpers'
 
 describe('MPDP Details page tests', () => {
-	test('GET /details route returns summary', async () => {
-		const searchString = 'Carter'
-		const page = '1'
+	let res: any
+	let $: cheerio.CheerioAPI
+	const searchString = 'Carter'
+	const page = '1'
+	
+	beforeEach(async () => {
+		if(res) { return }
+	
 		const options = getOptions('details', 'GET', {
 			payeeName: 'Carter',
 			searchString,
 			page
 		})
 
-		const res = await global.__SERVER__.inject(
-			options
-		)
+		res = await global.__SERVER__.inject(options)
+		$ = cheerio.load(res.payload)
+	})
 
-		expect(res.statusCode).toBe(200)
-		const $ = cheerio.load(res.payload)
+	afterAll(() => {
+		jest.resetAllMocks()
+	})
+
+	test('GET /details returns status 200', () => {
+    expect(res.statusCode).toBe(200)
+  })
+
+	test('Check for common elements to be present', () => {
 		expect($('h1').text()).toContain(searchString)
+		expect($('#mpdpSummaryPanel')).toBeDefined()
+		expect($('#mpdpSummaryBreakdown')).toBeDefined()
+		expect($('#mpdpMoreActions')).toBeDefined()
 		expect($('#reportProblem')).toBeDefined()
+	})
 
-		const linkObjs = [
-			{id: '#detailsBackLink', href: `/results?searchString=${searchString}&page=${page}`, text: 'Back to results'},
-			{id: '#callCharges', href: 'https://www.gov.uk/call-charges', text: 'Find out more about call charges'},
-			{id: '#sfiQueryForm', href: 'https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1088945/SFI-query-form.pdf', text: 'query form'}
-		]
+	test.each([
+		{id: '#detailsBackLink', href: `/results?searchString=${searchString}&page=${page}`, text: 'Back to results'},
+		{id: '#detailsBtmBackLink', href: `/results?searchString=${searchString}&page=${page}`, text: 'Back to results'},
+		{id: '#callCharges', href: 'https://www.gov.uk/call-charges', text: 'Find out more about call charges'},
+		{id: '#sfiQueryForm', href: 'https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1088945/SFI-query-form.pdf', text: 'query form'},
+		{id: '#newSearchLink', href: '/search', text: 'start a new search'},
+		{id: '#printLink', href: 'window.print()', text: 'print this page'}
+	])('All links are present', async ({id, href, text}) => {
+		
+		const linkElement = $(id)
+		expect(linkElement).toBeDefined()
+		expect(linkElement.attr('href')).toContain(href)
+		expect(linkElement.text()).toMatch(text)
+	})
 
-		linkObjs.forEach(obj => {
-			const link = $(obj.id)
-			expect(link).toBeDefined()
-			expect(link.attr('href')).toContain(obj.href)
-			expect(link.text()).toMatch(obj.text)
-		})
-
+	test('Check for header and footer', () => {
 		expectPhaseBanner($)
 		expectFooter($)
 		expectHeader($)
