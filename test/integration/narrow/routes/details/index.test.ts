d0162a634d7683673b951ff2aa2342de14deec81
Siddharth Godhania <siddharth.godhania@capgemini.com>
2023-01-09 11:31:23 +0000

    MPDP-320 Fixing SFI Query form link

--- test/integration/narrow/routes/details/index.test.ts
+++ test/integration/narrow/routes/details/index.test.ts
@@ -50,7 +50,7 @@ describe('MPDP Details page tests', () => {
 		{id: '#detailsBackLink', href: `/results?searchString=${searchString}&page=${page}`, text: 'Back to results'},
 		{id: '#detailsBtmBackLink', href: `/results?searchString=${searchString}&page=${page}`, text: 'Back to results'},
 		{id: '#callCharges', href: 'https://www.gov.uk/call-charges', text: 'Find out more about call charges'},
-		{id: '#sfiQueryForm', href: 'https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1088945/SFI-query-form.pdf', text: 'query form'},
+		{id: '#sfiQueryForm', href: 'https://www.gov.uk/government/publications/sustainable-farming-incentive-pilot-query-form', text: 'query form'},
 		{id: '#newSearchLink', href: '/search', text: 'start a new search'},
 		{id: '#printLink', href: 'window.print()', text: 'print this page'}
 	])('All links are present', async ({id, href, text}) => {
