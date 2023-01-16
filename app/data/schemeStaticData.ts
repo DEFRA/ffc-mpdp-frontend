bd7d7cd0e664182943fed0b2e817fec956693060
Siddharth Godhania <siddharth.godhania@capgemini.com>
2023-01-16 14:09:15 +0000

    MPDP-356 Updating the scheme descriptions

--- app/data/schemeStaticData.ts
+++ app/data/schemeStaticData.ts
@@ -1,12 +1,12 @@
 export const schemeStaticData = [
 	{
 		name: 'Sustainable Farming Incentive pilot',
-		description: 'Helping farmers manage land to improve food production that is more environmentally sustainable.',
+		description: 'Funding for farmers and land managers to improve productivity and efficiency for farming, horticultural and forestry businesses.',
 		link: 'https://www.gov.uk/government/collections/sustainable-farming-incentive-guidance'
 	},
 	{
 		name: 'Farming Equipment and Technology Fund',
-		description: 'To improve productivity and efficiency for farming, horticultural and forestry businesses.',
+		description: 'Grants to help farmers, horticulturalists and forestry owners invest in new technology.',
 		link: 'https://www.gov.uk/guidance/farming-equipment-and-technology-fund-round-1-manual'
 	}
 ]
