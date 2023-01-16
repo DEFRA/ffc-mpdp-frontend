df56bd8edfac0a8b3afb85b533e341c5bd5060d9
Siddharth Godhania <110030290+SidG-CG@users.noreply.github.com>
2023-01-16 15:55:40 +0000

    MPDP 339 Updating titles (#47)
    * MPDP-339 Adding title to all pages and adding tests
    
    * MPDP-339 Updated version number

--- test/integration/narrow/routes/accessibility/accessibility.test.ts
+++ test/integration/narrow/routes/accessibility/accessibility.test.ts
@@ -1,8 +1,17 @@
 import { getOptions } from '../../../../utils/helpers'
+import * as cheerio from 'cheerio'
+import { getPageTitle } from '../../../../../app/utils/helper'
+import { expectTitle } from '../../../../utils/title-expect'
 
 describe('MPDP Accessibility page test', () => {
-  test('GET /accessibility route returns 200', async () => {
+  const path = '/accessibility'
+  test(`GET ${path} route returns 200`, async () => {
     const res = await global.__SERVER__.inject(getOptions('accessibility'))
     expect(res.statusCode).toBe(200)
+
+    const $ = cheerio.load(res.payload)
+    const pageTitle = getPageTitle(path)
+    expect($('h1').text()).toMatch(pageTitle)
+    expectTitle($, pageTitle)
   })
 })
