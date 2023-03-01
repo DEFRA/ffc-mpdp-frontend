b29d7c7917856e496dd79620937ae0bdc621acb9
Siddharth Godhania <110030290+SidG-CG@users.noreply.github.com>
2023-03-01 11:10:05 +0000

    MPDP-46 Filter by County (#64)
    * MPDP-46 Adding filter by counties in FE

--- test/integration/narrow/routes/search/results.test.ts
+++ test/integration/narrow/routes/search/results.test.ts
@@ -2,7 +2,7 @@ import * as cheerio from 'cheerio'
 import { expectFooter } from '../../../../utils/footer-expects'
 import { expectHeader } from '../../../../utils/header-expects'
 import { expectPhaseBanner } from '../../../../utils/phase-banner-expect'
-import { getOptions, mockGetPaymentData, filterByAmounts, filterBySchemes } from '../../../../utils/helpers'
+import { getOptions, mockGetPaymentData, filterByAmounts, filterBySchemes, filterByCounties } from '../../../../utils/helpers'
 import mockData from '../../../../data/mockResults'
 import config from '../../../../../app/config'
 import { expectTitle } from '../../../../utils/title-expect'
@@ -435,7 +435,7 @@ describe('GET /results route with amounts parameter works', () => {
 
     let dataMatchingSchemesAndAmounts = filterBySchemes(mockData, schemes)
     dataMatchingSchemesAndAmounts = filterByAmounts(dataMatchingSchemesAndAmounts, amounts)
-    console.log(dataMatchingSchemesAndAmounts.length)
+    
     const $ = cheerio.load(res.payload)
     
     $('a.govuk-link.govuk-link--no-visited-state').each((_i, elem) => {
@@ -443,5 +443,64 @@ describe('GET /results route with amounts parameter works', () => {
     })
     
     expect($('#totalResults').text()).toMatch(`${dataMatchingSchemesAndAmounts.length} results`)
+  })
+})
+
+describe('GET /results route with counties parameter works', () => {
+  const searchString = 'Sons'
+
+  afterAll(() => {
+    jest.resetAllMocks()
+  })
+
+  test('Get /result works with single county in query params', async () => {
+    const schemes = 'Farming Equipment and Technology Fund'
+    const counties = 'Durham'
+    const res = await global.__SERVER__.inject(
+      getOptions(
+        'results',
+        'GET',
+        { 
+          searchString, 
+          schemes,
+          counties
+        }
+      )
+    )
+
+    let filteredData = filterBySchemes(mockData, [schemes])
+    filteredData = filterByCounties(filteredData, [counties])
+
+    const $ = cheerio.load(res.payload)
+
+    $('a.govuk-link.govuk-link--no-visited-state').each((_i, elem) => {
+      expect(filteredData.find((x: any) => x.payee_name === $(elem).text()))
+    })
+
+    expect($('#totalResults').text()).toMatch(`${filteredData.length} results`)
+  })
+
+  test('Get /results work with multiple amounts in query params', async () => {
+    const schemes = ['Sustainable Farming Incentive pilot', 'Farming Equipment and Technology Fund']
+    const counties = ['Durham', 'Berkshire']
+    const options = getOptions(
+      'results',
+      'GET',
+      { searchString },
+      { schemes, counties }
+    )
+    
+    const res = await global.__SERVER__.inject(options)
+
+    let dataMatchingSchemesAndCounties = filterBySchemes(mockData, schemes)
+    dataMatchingSchemesAndCounties = filterByCounties(dataMatchingSchemesAndCounties, counties)
+    
+    const $ = cheerio.load(res.payload)
+    
+    $('a.govuk-link.govuk-link--no-visited-state').each((_i, elem) => {
+      expect(dataMatchingSchemesAndCounties.find((x: any) => x.payee_name === $(elem).text()))
+    })
+    
+    expect($('#totalResults').text()).toMatch(`${dataMatchingSchemesAndCounties.length} results`)
   })
 })
