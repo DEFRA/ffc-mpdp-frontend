06c1e4d4dc5c0b72c914e4bb0e325b5fbb224853
vmuthu <112011276+vmuthu-kn@users.noreply.github.com>
2023-04-06 12:10:10 +0100

    mpdp-25 csv download details page (#72)
    * added download link to details page
    
    * download all csv details page
    
    * formated code
    
    * updated test cases
    
    * updated review comments
    
    * fixed the amount range value for MPDP-458
    
    * updated review comments
    
    * updated review comments
    
    * updated the test cases
    
    * updated test case
    
    * updated more test cases

---
+++ app/routes/downloaddetails.ts
@@ -0,0 +1,27 @@
+import { Request, ResponseToolkit, ResponseObject } from "@hapi/hapi";
+import { getDownloadDetailsCsv } from "../backend/api";
+import Joi from "joi";
+
+module.exports = {
+  method: 'GET',
+  path: '/downloaddetails',
+  options: {
+    auth: false,
+    validate: {
+      query: Joi.object({
+        payeeName: Joi.string().trim().required(),
+        partPostcode: Joi.string().trim().required()
+      }),
+      failAction: async (request: Request, h: ResponseToolkit, error: any) => {
+        h.response(error.toString()).code(400).takeover()
+      }
+    },
+    handler: async (request: Request, h: ResponseToolkit) => {
+      const {payeeName, partPostcode} = request.query
+      const content = await getDownloadDetailsCsv(payeeName, partPostcode)
+      return h.response(content)
+          .type('application/csv')
+          .header('Content-Disposition', 'attachment; filename=\"ffc-payment-details.csv\"')
+    }
+  }
+}
