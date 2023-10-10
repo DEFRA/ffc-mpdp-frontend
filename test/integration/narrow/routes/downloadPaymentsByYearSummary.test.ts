57cebf2dca3cd0f546e33d6cfbc02b3a0efcd24d
Afshin <afshin.karamifar@capgemini.com>
2023-10-10 16:38:04 +0100

    Download CSV Link for Payments by Year

---
+++ test/integration/narrow/routes/downloadPaymentsByYearSummary.test.ts
@@ -0,0 +1,40 @@
+import * as utils from '../../../../app/utils';
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('downloadPaymentsByYearSummary csv test', () => {
+  const content = 'Sample data in csv';
+  const mockedFetch = jest.spyOn(utils, 'getBuffer');
+  mockedFetch.mockResolvedValue(new Buffer(content));
+  const request = {
+    method: 'GET',
+    url: '/downloadPaymentsByYearSummary',
+  };
+
+  test('GET /downloadPaymentsByYearSummary returns 200', async () => {
+    const res = await global.__SERVER__.inject(request);
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('GET /downloadPaymentsByYearSummary returns attachment', async () => {
+    const res = await global.__SERVER__.inject(request);
+    expect(res.headers).toHaveProperty('content-type', 'application/csv');
+    expect(res.headers).toHaveProperty(
+      'content-disposition',
+      'attachment; filename="ffc-year-payments-summary.csv"'
+    );
+  });
+
+  test('GET /downloadPaymentsByYearSummary returns the expected content', async () => {
+    const res = await global.__SERVER__.inject(request);
+    expect(res.result).toBe(content);
+  });
+
+  test('GET /downloadPaymentsByYearSummary throws error when underlying error', async () => {
+    mockedFetch.mockRejectedValue('Internal Server Error');
+    const res = await global.__SERVER__.inject(request);
+    expect(res.statusCode).toBe(500);
+  });
+});
