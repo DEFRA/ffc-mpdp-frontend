244155217f1b2f4cd1bd2ae29fcd50f66ff8ce72
Afshin-Karamifar-Capgemini <afshin.karamifar@capgemini.com>
2023-10-20 17:23:18 +0100

    MPDP-554 Accessibility content has been add

--- app/routes/accessibility.ts
+++ app/routes/accessibility.ts
@@ -4,6 +4,9 @@ module.exports = {
   method: 'GET',
   path: '/accessibility',
   handler: (_request: Request, h: ResponseToolkit): ResponseObject => {
-    return h.view('accessibility/accessibility-policy')
+    return h.view('accessibility/accessibility-policy', {
+      referer: _request.headers.referer,
+      // relatedContentData: getRelatedContentLinks('accessibility-policy'),
+    });
   }
 }
