4791ea2d0dfff88dd26fd94a0846cce8bb274c26
Siddharth Godhania <siddharth.godhania@capgemini.com>
2024-02-05 17:48:36 +0000

    MPDP-571 Fixing download results link with years filter

--- test/integration/narrow/routes/downloadresults.test.ts
+++ test/integration/narrow/routes/downloadresults.test.ts
@@ -11,21 +11,27 @@ describe('downloadresults csv test', () => {
       part_postcode: 'B61',
       town: 'Bromsgrove, unparished area',
       county_council: 'Worcestershire',
-      total_amount: '2750.00'
+      year: '21/22',
+      total_amount: '2750.00',
+      scheme: 'Farming Investment Fund'
     },
     {
       payee_name: 'John Rolfson',
       part_postcode: 'BH6',
       town: 'Bournemouth, Christchurch and Poole, unparished area',
       county_council: 'None',
-      total_amount: 22159
+      year: '21/22',
+      total_amount: 22159,
+      scheme: 'Farming Investment Fund'
     },
     {
       payee_name: 'Mathew Johnston',
       part_postcode: 'WA14',
       town: 'Trafford, unparished area',
       county_council: 'None',
-      total_amount: 13285
+      year: '22/23',
+      total_amount: 13285,
+      scheme: 'Farming Resilience Fund'
     }],
     total: 3,
     filterOptions: {}
@@ -65,6 +71,16 @@ describe('downloadresults csv test', () => {
     const res = await global.__SERVER__.inject(request)
     expect(res.result).toContain("payee_name\",\"part_postcode\",\"town\",\"county_council\",\"amount")
     expect(res.result).toContain("John Rolfson\",\"BH6\",\"Bournemouth, Christchurch and Poole, unparished area\",\"None\",\"22,159.00")
+  })
+
+  test('GET /downloadresults returns the expected content when years filters selected', async () => {
+    const request = {
+      method: 'GET',
+      url: 'http://localhost:3001/downloadresults?searchString=john&years=22/23&sortBy=score'
+    }
+    const res = await global.__SERVER__.inject(request)
+    expect(res.result).toContain("payee_name\",\"part_postcode\",\"town\",\"county_council\",\"amount")
+    expect(res.result).toContain("Mathew Johnston\",\"WA14\",\"Trafford, unparished area\",\"None\",\"13,285.00")
   })
 
 })
