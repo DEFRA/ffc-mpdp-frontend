19388e4be94e35622b37700c7a1e8941f4c014fe
Siddharth Godhania <siddharth.godhania@capgemini.com>
2024-02-21 10:32:55 +0000

    MPDP-573 Added confliuence page link to the solution

--- app/routes/downloadAllData.ts
+++ app/routes/downloadAllData.ts
@@ -2,6 +2,9 @@ import { Request, ResponseToolkit } from "@hapi/hapi";
 
 import { getAllPaymentDataFilePath } from '../utils/helper'
 
+/* Checkout this confluence page to find out the rationale behind this functionality
+ / https://eaflood.atlassian.net/wiki/spaces/MAKING/pages/4721999958/Download+all+data+functionality+overview 
+*/
 module.exports = {
   method: 'GET',
   path: '/downloadalldata',
