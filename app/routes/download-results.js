ef7d329f9a340a91e84ad04307546bd4d3e3ccee
John Watson <42574977+johnwatson484@users.noreply.github.com>
2024-11-19 16:29:38 +0000

    Use new CSV endpoint (#184)

--- app/routes/download-results.js
+++ app/routes/download-results.js
@@ -1,48 +1,33 @@
-const { getPaymentData } = require('../backend/api')
-const { getReadableAmount } = require('../utils/helper')
+const { post } = require('../backend/api')
 const { resultsQuery: query } = require('./queries/search/results')
-const { Parser } = require('json2csv')
 
-module.exports = [
-  {
-    path: '/downloadresults',
-    method: 'GET',
-    options: {
-      auth: false,
-      validate: {
-        query,
-        failAction: async (_request, h, error) => {
-          h.response(error.toString()).code(400).takeover()
-        }
-      },
-      handler: async (request, h) => {
-        const csvFields = [
-          'payee_name',
-          'part_postcode',
-          'town',
-          'county_council',
-          'amount'
-        ]
-        const action = 'download'
-        const offset = 0
-        const limit = -1
-        const { searchString, schemes, amounts, counties, years } = request.query
-        const sortBy = decodeURIComponent(request.query.sortBy)
-        const filterBy = {
-          schemes: typeof schemes === 'string' ? [schemes] : schemes,
-          amounts: typeof amounts === 'string' ? [amounts] : amounts,
-          counties: typeof counties === 'string' ? [counties] : counties,
-          years: typeof years === 'string' ? [years] : years
-        }
-
-        const { results } = await getPaymentData(searchString, offset, filterBy, sortBy, action, limit)
-        const matches = results.map(x => ({ ...x, amount: getReadableAmount(parseFloat(x.total_amount)) }))
-        const csvParser = new Parser({ fields: csvFields })
-        const csv = csvParser.parse(matches)
-        return h.response(csv)
-          .type('application/csv')
-          .header('Content-Disposition', 'attachment; filename="ffc-payment-results.csv"')
+module.exports = {
+  path: '/downloadresults',
+  method: 'GET',
+  options: {
+    auth: false,
+    validate: {
+      query,
+      failAction: async (_request, h, error) => h.response(error.toString()).code(400).takeover()
+    },
+    handler: async (request, h) => {
+      const { searchString, schemes, amounts, counties, years } = request.query
+      const sortBy = decodeURIComponent(request.query.sortBy)
+      const filterBy = {
+        schemes: typeof schemes === 'string' ? [schemes] : schemes,
+        amounts: typeof amounts === 'string' ? [amounts] : amounts,
+        counties: typeof counties === 'string' ? [counties] : counties,
+        years: typeof years === 'string' ? [years] : years
       }
+
+      const content = await post('/file', { searchString, filterBy, sortBy })
+      return h
+        .response(content?.payload)
+        .type('application/csv')
+        .header(
+          'Content-Disposition',
+          'attachment; filename="ffc-payment-results.csv"'
+        )
     }
   }
-]
+}
