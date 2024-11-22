2d281d2cb94832f18a212b2b34c484e07294969d
John Watson <john.watson1@defra.gov.uk>
2024-11-22 12:44:54 +0000

    Update tests

--- app/routes/download-all-data.js
+++ app/routes/download-all-data.js
@@ -1,6 +1,4 @@
-const { Readable } = require('stream')
 const { getDownloadAllCsv } = require('../backend/api')
-const { getAllPaymentDataFilePath } = require('../utils/helper')
 
 /* Checkout this confluence page to find out the rationale behind this functionality
  / https://eaflood.atlassian.net/wiki/spaces/MAKING/pages/4721999958/Download+all+data+functionality+overview
@@ -10,13 +8,8 @@ module.exports = {
   path: '/downloadalldata',
   handler: async (_request, h) => {
     const csvStream = await getDownloadAllCsv()
-
     return h.response(csvStream)
       .type('text/csv')
       .header('Content-Disposition', 'attachment; filename="ffc-payment-data.csv"')
-    // return h.file(getAllPaymentDataFilePath(), {
-    //   mode: 'attachment',
-    //   filename: 'ffc-payment-data.csv'
-    // })
   }
 }
